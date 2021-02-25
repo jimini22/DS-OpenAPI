5a8054cbea74a24b7bddc9525562d1729452872d
minheeJI (jimini@bluedigm.com) <minheeJI (jimini@bluedigm.com)@LAPTOP-UOHQ7FDV>
2021-02-25 10:10:15 +0900

    initial commit. start!

---
+++ src/mian/java/com/portal/common/service/CommonCodeService.java
@@ -0,0 +1,15 @@
+package com.portal.common.service;
+
+import java.util.Map;
+
+
+/**
+ * The interface Common code service.
+ */
+public interface CommonCodeService {
+
+	
+	public Map<String,Object> selectCommonCodeList()  throws Exception;
+
+	
+}
